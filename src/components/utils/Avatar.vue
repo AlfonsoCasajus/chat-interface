0c7f8e70093304a1ee0d5f930f42c54e7a99ea38
AlfonsoCasajus <alfonso.casajus@hotmail.com>
2024-08-02 11:10:32 -0300

    initial-commit

---
+++ src/components/utils/Avatar.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="w-16 h-16 bg-blue-500 text-white flex items-center justify-center rounded-full text-3xl">
+		{{ name[0].toUpperCase() }}
+	</div>
+</template>
+  
+  <script setup>
+ 
+  const props = defineProps({
+    name: String
+  })
+  
+  </script>
+  
+  <style scoped>
+  </style>
+  

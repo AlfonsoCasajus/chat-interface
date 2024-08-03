27a6962b11475d0d107e84b19db6ddebe4631246
AlfonsoCasajus <alfonso.casajus@hotmail.com>
2024-08-03 11:59:12 -0300

    add vuex

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from "vuex"
+import conversations from "./modules/conversations";
+
+const store = createStore({
+	modules: {
+	  conversations
+	}
+})
+
+export default store;
